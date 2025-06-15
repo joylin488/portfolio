aa178c5cecc3cf5235168fa3fa9ba12120a83614
joylin488 <jmwalog.student@asiancollege.edu.ph>
2025-06-15 09:51:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Function to display a welcome message
+function displayWelcomeMessage() {
+  alert("Welcome to Joylin Walog's Portfolio!");
+}
+
+// Call the function when the window loads
+window.onload = displayWelcomeMessage;
